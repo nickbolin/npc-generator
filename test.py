d2e63cdc6a5a165e8acc62e6a6024199202688d1
Nick Bolin <nbolin@vt.edu>
2015-12-27 01:30:49 -0500

    Vastly improved test driver, can now specify settings from command line and run premade tests

--- test.py
+++ test.py
@@ -1,32 +1,84 @@
 #!/usr/local/bin/python
+import argparse, sys
 import npc
 
-print "Generating a random NPC with complete default settings:"
-mynpc = npc.NonPC()
-mynpc.generate_npc()
-print mynpc 
-print
+def run_test(gender=None, race=None, commoner=None, level=None):
+	header = "Generating a random "	# always start the same way
+	if gender is not None:		# print gender, if provided
+		header += gender + " "
+	if level is not None:		# include level if provided
+		header += "level " + str(level) + " "
+	if race is not None:		# specify race if provided
+		header += race + " "
+	header += "NPC with " 		# always include this
+	if commoner is not None:
+		header += ('commoner' if commoner else 'adventurer') + " "
+	else:
+		header += "default "
+	header += "settings:"
+	print header
+	current_npc = npc.NonPC()
 
-print "Generating a random Elf NPC with adventurer settings:"
-elfadv = npc.NonPC()
-elfadv.generate_npc(commoner=False, race='Elf')
-print elfadv 
-print
+	current_npc.generate_npc(commoner=commoner, race=race, gender=gender, level=level)
 
-print "Generating a random Dwarf NPC with adventurer settings:"
-dwarfadv = npc.NonPC()
-dwarfadv.generate_npc(commoner=False, race='Dwarf')
-print dwarfadv 
-print
+	print current_npc
+	print
 
-print "Generating a random Gnome NPC with adventurer settings:"
-gnomeadv = npc.NonPC()
-gnomeadv.generate_npc(commoner=False, race='Gnome')
-print gnomeadv 
-print
+def run_basic_test():
+	run_test()
+	run_test(race='Half-elf')
+	run_test(gender='Female')
+	run_test(commoner=False)
+	run_test(level=21)
+	run_test(race='Dwarf', gender='Male', commoner=False, level=29)
 
-print "Generating a male random NPC with commoner settings:"
-malecom = npc.NonPC()
-malecom.generate_npc(commoner=True, gender='Male')
-print malecom 
-print
+
+parser = argparse.ArgumentParser(description='A test driver for npc.py')
+# add arguments for npc customization
+parser.add_argument('-r', '--race', help='specify the race of the npc to generate')
+parser.add_argument('-g', '--gender', help='specify the gender of the npc to generate (Male/Female)')
+parser.add_argument('-l', '--level', help='specify the level of the npc to generate', type=int)
+parser.add_argument('-c','--commoner', help='specify that the npc is a commoner', action='store_true')
+parser.add_argument('-a', '--adventurer', help='specify that the npc is an adventurer', action='store_true')
+# add some utility arguments
+parser.add_argument('--count', help='number of characters to generate with these settings', type=int)
+parser.add_argument('--basic', help='run a basic series of tests', action='store_true')
+
+args = parser.parse_args()
+if args.count:				# get the number of loops, set default to 1
+	count = args.count
+else:
+	count = 1
+
+if args.basic:				# run a basic test as many times as count dictates
+	for x in range(count):
+		run_basic_test()
+	sys.exit()
+
+# otherwise, run a regular test with appropriate fields
+
+if args.race:				# set the race, default to None
+	npc_race = args.race
+else:
+	npc_race = None
+
+if args.gender:				# set the gender, default to None
+	npc_sex = args.gender
+else:
+	npc_sex = None
+
+if args.level:				# set the level, default to None
+	npc_level = args.level
+else:
+	npc_level = None
+
+if args.commoner:			# set the commoner attribute, default to None
+	npc_common = True
+elif args.adventurer:
+	npc_common = False
+else:
+	npc_common = None
+
+# run the appropriate number of tests
+for x in range(count):
+	run_test(gender=npc_sex, race=npc_race, level=npc_level, commoner=npc_common)
